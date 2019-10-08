2ea1d620a947ce48f1be81660345bfb416c6af25
Billy <billyjcatchpole@gmail.com>
2019-10-08 20:14:46 +0100

    1 - Learned about react-native flexbox, stylesheets and components including buttons, text and views

--- App.js
+++ App.js
@@ -1,19 +1,55 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+
+  //first element is the output/new text, second element is the function that changes the text
+  //Parameter of useState is the starting text
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+    <View style={styles.screen}> 
+
+      <View style={styles.firstView}>
+        
+        <TextInput placeholder="Course goal" style={styles.firstView_textInput}/>
+
+        <Button title="ADD" />
+
+      </View>
+
+      <View>
+
+      </View>
+
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
+    screen:{
+
+      padding:50
+      
+    },
+
+    firstView: {
+
+      flexDirection: "row", 
+      justifyContent: "space-between", 
+      alignItems: "center"
+
+    },
+
+    firstView_textInput: {
+
+      width: "80%", 
+      borderColor: "black", 
+      borderWidth: 1, 
+      padding:10
+
+    }
+     
 });
